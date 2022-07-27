b3f00d96b8ef8e15e0c8bdb1b20605aebaa6b685
melekalpay <melekalpy7@gmail.com>
2022-07-28 00:52:52 +0300

    step 2

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,5 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
+(function (){
 
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
@@ -22,3 +23,4 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+})();
